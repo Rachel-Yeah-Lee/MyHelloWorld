957f528ce29ed1cbc5549db6a27861d0e65f1546
Rachel-Yeah-Lee <rachel.genlee@gmail.com>
2018-04-06 11:10:15 +0800

    add Console.Wrtie

---
+++ MyHello_World/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
